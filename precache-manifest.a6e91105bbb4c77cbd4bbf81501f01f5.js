77f4907a606e9cdd3c8d6cd6a184aabe8aa6ab8e
Marcelo Severgnini <marcelo.severgnini@diconium.com>
2020-02-14 18:53:20 +0000

    Updates

---
+++ precache-manifest.a6e91105bbb4c77cbd4bbf81501f01f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19858e2f1d03f74fa3960bce24a5dd76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c11d3610db986b9a49cb",
+    "url": "/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "40eabae0d3ee82c87ec7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c11d3610db986b9a49cb",
+    "url": "/static/js/2.ef64b4e1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ef64b4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40eabae0d3ee82c87ec7",
+    "url": "/static/js/main.03362a00.chunk.js"
+  },
+  {
+    "revision": "2fafcf32258e33fa1618",
+    "url": "/static/js/runtime-main.e6c5523e.js"
+  }
+]);
