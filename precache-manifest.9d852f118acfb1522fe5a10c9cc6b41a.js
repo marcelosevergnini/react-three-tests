ad11165364c66965d2fbb005690817dcf30e674c
Marcelo Severgnini <marcelo.severgnini@diconium.com>
2020-02-24 07:47:43 +0000

    Updates

---
+++ precache-manifest.9d852f118acfb1522fe5a10c9cc6b41a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d196c15ed6f72740169c2481adfd0f8d",
+    "url": "/react-three-tests/index.html"
+  },
+  {
+    "revision": "f0273f8e887cba53d4d5",
+    "url": "/react-three-tests/static/css/2.4f498cb1.chunk.css"
+  },
+  {
+    "revision": "245fc02bf44ddc8a7580",
+    "url": "/react-three-tests/static/css/main.0af865a1.chunk.css"
+  },
+  {
+    "revision": "f0273f8e887cba53d4d5",
+    "url": "/react-three-tests/static/js/2.292252db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-three-tests/static/js/2.292252db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "245fc02bf44ddc8a7580",
+    "url": "/react-three-tests/static/js/main.283596f3.chunk.js"
+  },
+  {
+    "revision": "bd83a38a961d40470a91",
+    "url": "/react-three-tests/static/js/runtime-main.b05dd656.js"
+  },
+  {
+    "revision": "3b2a3ec0aaa6c2bc14d289e37e879ad2",
+    "url": "/react-three-tests/static/media/arch-logo.3b2a3ec0.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-three-tests/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-three-tests/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-three-tests/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-three-tests/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-three-tests/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.01b191328a15ed66099d736903b6db9f.js"
+  "/react-three-tests/precache-manifest.9d852f118acfb1522fe5a10c9cc6b41a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-three-tests/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
